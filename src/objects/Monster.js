dca30f3a7ae7352bc854a09e2e15512c732935f6
Dixon Cheng <mail@dixoncheng.com>
2020-09-10 23:32:34 +1200

    Custom font, typing

--- src/objects/Monster.js
+++ src/objects/Monster.js
@@ -2,10 +2,46 @@ export default class Monster extends Phaser.GameObjects.Container {
   constructor(scene, x, y, texture, name, velocity) {
     super(scene, x, y);
     this.name = name;
+    this.correctCount = 0;
 
-    const image = new Phaser.GameObjects.Image(scene, 0, 0, texture);
+    // image
+    const image = new Phaser.GameObjects.Image(scene, 0, 0, texture).setScale(
+      1.5
+    );
     this.add([image]);
+
+    // text
+    // const text = new Phaser.GameObjects.Text(scene, 0, 36, name.toUpperCase(), {
+    //   fill: 'white'
+    // });
+    const text = new Phaser.GameObjects.DynamicBitmapText(
+      scene,
+      0,
+      36,
+      'carrier_command',
+      name,
+      18
+    )
+      .setOrigin(0.5, 0)
+      .setDisplayCallback((conf) => {
+        // console.log(conf);
+        if (conf.index < this.correctCount) {
+          return {
+            ...conf,
+            color: 0x97d897
+            // rotation: 0.1
+            // scale: conf.scale * 1.1
+          };
+        }
+        return { ...conf, color: 0xffffff };
+      });
+    this.add([text]);
+
+    // movement
     scene.physics.world.enable(this);
-    this.body.setVelocity(0, velocity);
+    // this.body.setVelocity(0, velocity);
+
+    // typing
+    // scene.events.on('typing', this.typing, this);
   }
 }
--- src/scenes/game.js
+++ src/scenes/game.js
@@ -6,7 +6,8 @@ import monsters from '../data/list.json';
 
 const imagePath =
   // 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/platinum/';
-  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/emerald/';
+  // 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/emerald/';
+  'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iv/platinum/';
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
@@ -16,6 +17,11 @@ export default class GameScene extends Phaser.Scene {
   preload() {
     this.load.image('sky', skyImg);
     this.load.image('test', `${imagePath}1.png`);
+    this.load.bitmapFont(
+      'carrier_command',
+      'https://raw.githubusercontent.com/photonstorm/phaser-examples/master/examples/assets/fonts/bitmapFonts/carrier_command.png',
+      'https://raw.githubusercontent.com/photonstorm/phaser-examples/master/examples/assets/fonts/bitmapFonts/carrier_command.xml'
+    );
   }
 
   create() {
@@ -27,10 +33,10 @@ export default class GameScene extends Phaser.Scene {
     this.monsters = this.add.group();
     const monster = new Monster(
       this,
-      100,
+      200,
       100,
       'test',
-      'bulbasaur',
+      String('bulbasaur').toUpperCase(),
       Phaser.Math.Between(30, 100)
     );
 
@@ -39,6 +45,25 @@ export default class GameScene extends Phaser.Scene {
     // create monster class
     // appear
     // typing bubble appear
+
+    this.input.keyboard.on('keyup', this.keyPress, this);
+  }
+
+  keyPress(event) {
+    // todo check if currently started a word
+    // else find monster with the correct first letter, begin word
+
+    // this.events.emit('typing', event.keyCode);
+    const monster = this.monsters.children.entries[0]; //TEMP
+    const { name, correctCount } = monster;
+    if (name.charCodeAt(correctCount) === event.keyCode) {
+      monster.correctCount += 1;
+      if (monster.correctCount >= name.length) {
+        // word is finished
+        // play exit animation
+        // remove from group
+      }
+    }
   }
 
   update() {}
